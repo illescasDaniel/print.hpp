a6cbeb44f67de4c9eebe4a23216475871183c428
illescasDaniel <illescas.daniel@protonmail.com>
2016-09-09 13:46:58 +0200

    Initial Commit

---
+++ Print/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Print
+//
+//  Created by Daniel Illescas Romero on 09/09/2016.
+//  Copyright © 2016 Daniel Illescas Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
